d6fc5030dbadacb185adf5badab9f341ed82d527
Dominik Urbani <dominikurbani@gmx.de>
2021-12-08 16:53:52 +0100

    First Project

---
+++ Assets/Scripts/MoveToGoalAgent.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
+
+public class MoveToGoalAgent : Agent
+{
+    [SerializeField] private Transform targetTransform;
+    [SerializeField] private Material winMaterial;
+    [SerializeField] private Material loseMaterial;
+    [SerializeField] private MeshRenderer floorMeshRenderer;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.localPosition = new Vector3(Random.Range(-1.5f, 1.5f), 0.51f, Random.Range(-4f, 0));
+        targetTransform.localPosition = new Vector3(Random.Range(-1.5f, 1.5f), 0.5f, Random.Range(1f, 4));
+    }
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetTransform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float moveX = actions.ContinuousActions[0];
+        float moveZ = actions.ContinuousActions[1];
+        float moveSpeed = 4f;
+
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
+        continousActions[0] = Input.GetAxis("Vertical");
+        continousActions[1] = Input.GetAxis("Horizontal");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Target"))
+        {
+            floorMeshRenderer.material = winMaterial;
+            SetReward(1f);
+        }
+        else if (other.CompareTag("Wall"))
+        {
+            floorMeshRenderer.material = loseMaterial;
+            SetReward(-1f);
+        }
+        EndEpisode();
+    }
+}
