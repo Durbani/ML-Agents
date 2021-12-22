759df842716099bc1a6f464595777ad84ee4380f
ML <lemmrich@stud.hs-heilbronn.de>
2021-12-22 12:02:13 +0100

    final big level training

--- Assets/Scripts/RayPerceptionScripts/RayPerceptionAgent.cs
+++ Assets/Scripts/RayPerceptionScripts/RayPerceptionAgent.cs
@@ -34,6 +34,8 @@ public class RayPerceptionAgent : Agent
     [SerializeField] private Vector3[] possiblePositionsMonument;
     [SerializeField] private Vector3[] possiblePositionsIrrgarten;
 
+    public bool enableFinalLevel = false;
+
     private bool buttonPressed;
 
     private CameraSensorMovement movementScript;
@@ -61,100 +63,183 @@ public class RayPerceptionAgent : Agent
 
         //Debug.Log(Academy.Instance.EnvironmentParameters.GetWithDefault("level_data", 0f));
 
-        currentState = Academy.Instance.EnvironmentParameters.GetWithDefault("level_data", 0f);
-
-        if (currentState < 3.0f)
+        if (!enableFinalLevel)
         {
-            movementScript.ResetPlayer(new Vector3(-10.3f, 0.501f, -0.4f));
+
+            currentState = Academy.Instance.EnvironmentParameters.GetWithDefault("level_data", 0f);
+
+            if (currentState < 3.0f)
+            {
+                movementScript.ResetPlayer(new Vector3(-10.3f, 0.501f, -0.4f));
+            }
+            else
+            {
+                movementScript.ResetPlayer(new Vector3(Random.Range(-18.5f, -10.5f), 0.501f, Random.Range(-48f, 48f)));
+            }
+
+            if (currentState < 1.0f) //Stage 1
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
+
+                //Button
+                buttonTransform.localPosition = new Vector3(-16.5f, 1.334f, -4.3f);
+            }
+            else if (currentState < 2.0f) //Stage 2
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-18f, -14f), 1.25f, Random.Range(-13f, 13f));
+            }
+            else if (currentState < 3.0f) //Stage 3
+            {
+                if (outsideWall1.activeSelf)
+                {
+                    outsideWall1.SetActive(false);
+                    outsideWall2.SetActive(false);
+                    outsideWall3.SetActive(false);
+                    outsideWall4.SetActive(false);
+                }
+
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-18f, -14f), 1.25f, Random.Range(-24f, 24f));
+            }
+            else if (currentState < 4.0f) //Stage 4
+            {
+                if (outsideWallNextStage1.activeSelf)
+                {
+                    outsideWallNextStage1.SetActive(false);
+                    outsideWallNextStage2.SetActive(false);
+                    outsideWallNextStage3.SetActive(false);
+                    outsideWallNextStage4.SetActive(false);
+                }
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-18.5f, -11f), 1.25f, Random.Range(-48f, 48f));
+            }
+            else if (currentState < 5.0f) //Stage 5
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 0.2f), 1.25f, Random.Range(-48f, 48f));
+            }
+            else if (currentState < 6.0f) //Stage 6
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
+                targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 0.2f), 1.25f, Random.Range(-48f, 48f));
+            }
+            else if (currentState < 7.0f) //Stage 7
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
+                targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 20f), 1.25f, Random.Range(-48f, 48f));
+            }
+            else if (currentState < 8.0f) //Stage 8
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
+                targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
+
+                //Button
+                buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 47f), 1.25f, Random.Range(-48f, 48f));
+            }
+            else if (currentState < 9.0f) //Stage 9
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
+                targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
+
+                //Enable Obstacles
+                monumentObject.SetActive(true);
+                irrgartenObject.SetActive(true);
+
+                for (int i = 0; i < irrgartenObject.transform.childCount; i++)
+                {
+                    Transform wall = irrgartenObject.transform.GetChild(i);
+                    if (wall.gameObject.tag.Equals("Untagged"))
+                    {
+                        break;
+                    }
+                    wall.gameObject.tag = "Untagged";
+                    wall.gameObject.GetComponent<BoxCollider>().isTrigger = false;
+                }
+
+                //Randomize button position
+                float randomPosition = Random.Range(1f, 4f);
+
+                if (randomPosition < 2f)
+                {
+                    //Button
+                    buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 9f), 1.25f, Random.Range(-48f, 48f));
+                }
+                else if (randomPosition < 3f) //Button on monument
+                {
+                    buttonTransform.localPosition = possiblePositionsMonument[Mathf.RoundToInt(Random.Range(0f, possiblePositionsMonument.Length - 1))];
+                }
+                else //Button in irrgarten
+                {
+                    buttonTransform.localPosition = possiblePositionsIrrgarten[Mathf.RoundToInt(Random.Range(0f, possiblePositionsIrrgarten.Length - 1))];
+                }
+            }
+            else if (currentState < 10.0f) //Stage 10
+            {
+                //Box with Target
+                targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
+                targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
+
+                //Enable Obstacles
+                monumentObject.SetActive(true);
+                irrgartenObject.SetActive(true);
+
+                for (int i = 0; i < irrgartenObject.transform.childCount; i++)
+                {
+                    Transform wall = irrgartenObject.transform.GetChild(i);
+
+                    if (wall.gameObject.tag.Equals("Wall"))
+                    {
+                        break;
+                    }
+
+                    wall.gameObject.tag = "Wall";
+                    wall.gameObject.GetComponent<BoxCollider>().isTrigger = true;
+                }
+
+
+                //Randomize button position
+                float randomPosition = Random.Range(1f, 4f);
+
+                if (randomPosition < 2f)
+                {
+                    //Button
+                    buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 9f), 1.25f, Random.Range(-48f, 48f));
+                }
+                else if (randomPosition < 3f) //Button on monument
+                {
+                    buttonTransform.localPosition = possiblePositionsMonument[Mathf.RoundToInt(Random.Range(0f, possiblePositionsMonument.Length - 1))];
+                }
+                else //Button in irrgarten
+                {
+                    buttonTransform.localPosition = possiblePositionsIrrgarten[Mathf.RoundToInt(Random.Range(0f, possiblePositionsIrrgarten.Length - 1))];
+                }
+            }
         }
         else
-        {
-            movementScript.ResetPlayer(new Vector3(Random.Range(-18.5f, -10.5f), 0.501f, Random.Range(-48f, 48f)));
-        }
-
-        if (currentState < 1.0f) //Stage 1
-        {
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
-
-            //Button
-            buttonTransform.localPosition = new Vector3(-16.5f, 1.334f, -4.3f);
-        }
-        else if (currentState < 2.0f) //Stage 2
-        {
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-18f, -14f), 1.25f, Random.Range(-13f, 13f));
-        }
-        else if (currentState < 3.0f) //Stage 3
-        {
-            if (outsideWall1.activeSelf)
-            {
-                outsideWall1.SetActive(false);
-                outsideWall2.SetActive(false);
-                outsideWall3.SetActive(false);
-                outsideWall4.SetActive(false);
-            }
-
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-18f, -14f), 1.25f, Random.Range(-24f, 24f));
-        }
-        else if (currentState < 4.0f) //Stage 4
-        {
-            if (outsideWallNextStage1.activeSelf)
-            {
-                outsideWallNextStage1.SetActive(false);
-                outsideWallNextStage2.SetActive(false);
-                outsideWallNextStage3.SetActive(false);
-                outsideWallNextStage4.SetActive(false);
-            }
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(-26.7f, 0f, -0.2f);
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-18.5f, -11f), 1.25f, Random.Range(-48f, 48f));
-        }
-        else if (currentState < 5.0f) //Stage 5
-        {
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 0.2f), 1.25f, Random.Range(-48f, 48f));
-        }
-        else if (currentState < 6.0f) //Stage 6
-        {
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
-            targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 0.2f), 1.25f, Random.Range(-48f, 48f));
-        }
-        else if (currentState < 7.0f) //Stage 7
-        {
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
-            targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 20f), 1.25f, Random.Range(-48f, 48f));
-        }
-        else if (currentState < 8.0f) //Stage 8
-        {
-            //Box with Target
-            targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
-            targetBoxTransform.localRotation = Quaternion.Euler(Vector3.up * Random.Range(0f, 360f));
-
-            //Button
-            buttonTransform.localPosition = new Vector3(Random.Range(-8.4f, 47f), 1.25f, Random.Range(-48f, 48f));
-        }
-        else if (currentState < 9.0f) //Stage 9
         {
             //Box with Target
             targetBoxTransform.localPosition = new Vector3(Random.Range(-40f, -25f), 0f, Random.Range(-37f, 37f));
@@ -163,6 +248,33 @@ public class RayPerceptionAgent : Agent
             //Enable Obstacles
             monumentObject.SetActive(true);
             irrgartenObject.SetActive(true);
+
+            if (outsideWall1.activeSelf)
+            {
+                outsideWall1.SetActive(false);
+                outsideWall2.SetActive(false);
+                outsideWall3.SetActive(false);
+                outsideWall4.SetActive(false);
+            }
+
+            if (outsideWallNextStage1.activeSelf)
+            {
+                outsideWallNextStage1.SetActive(false);
+                outsideWallNextStage2.SetActive(false);
+                outsideWallNextStage3.SetActive(false);
+                outsideWallNextStage4.SetActive(false);
+            }
+
+            for (int i = 0; i < irrgartenObject.transform.childCount; i++)
+            {
+                Transform wall = irrgartenObject.transform.GetChild(i);
+                if (wall.gameObject.tag.Equals("Untagged"))
+                {
+                    break;
+                }
+                wall.gameObject.tag = "Untagged";
+                wall.gameObject.GetComponent<BoxCollider>().isTrigger = false;
+            }
 
             //Randomize button position
             float randomPosition = Random.Range(1f, 4f);
@@ -207,12 +319,19 @@ public class RayPerceptionAgent : Agent
     {
         AddReward(-0.0005f);
 
-        float moveInput = actions.DiscreteActions[2] <= 1 ? actions.DiscreteActions[2] : -1;
-        float turnInput = actions.DiscreteActions[3] <= 1 ? actions.DiscreteActions[3] : -1;
+        //Discrete Movement
+        //float moveInput = actions.DiscreteActions[2] <= 1 ? actions.DiscreteActions[2] : -1;
+        //float turnInput = actions.DiscreteActions[3] <= 1 ? actions.DiscreteActions[3] : -1;
+        //int pressButton = actions.DiscreteActions[0];
+        //bool jumpPressed = actions.DiscreteActions[1] == 1 ? true : false;
+
+        //Continues Movement
+        float moveInput = actions.ContinuousActions[0];
+        float turnInput = actions.ContinuousActions[1];
         int pressButton = actions.DiscreteActions[0];
         bool jumpPressed = actions.DiscreteActions[1] == 1 ? true : false;
 
-        //Debug.Log(actions.DiscreteActions[1]);
+        //Debug.Log(moveInput);
 
         movementScript.moveInput = moveInput;
         movementScript.turnInput = turnInput;
@@ -240,9 +359,15 @@ public class RayPerceptionAgent : Agent
     public override void Heuristic(in ActionBuffers actionsOut)
     {
         ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
+        ActionSegment<float> continousActions = actionsOut.ContinuousActions;
 
-        discreteActions[2] = Input.GetAxis("Vertical") >= 0 ? Mathf.RoundToInt(Input.GetAxis("Vertical")) : 2;
-        discreteActions[3] = Input.GetAxis("Horizontal") >= 0 ? Mathf.RoundToInt(Input.GetAxis("Horizontal")) : 2;
+        //Discrete
+        //discreteActions[2] = Input.GetAxis("Vertical") >= 0 ? Mathf.RoundToInt(Input.GetAxis("Vertical")) : 2;
+        //discreteActions[3] = Input.GetAxis("Horizontal") >= 0 ? Mathf.RoundToInt(Input.GetAxis("Horizontal")) : 2;
+
+        //continues move
+        continousActions[0] = Input.GetAxis("Vertical");
+        continousActions[1] = Input.GetAxis("Horizontal");
 
         discreteActions[0] = Input.GetKey(KeyCode.B) ? 1 : 0;
         discreteActions[1] = Input.GetButton("Jump") ? 1 : 0;
